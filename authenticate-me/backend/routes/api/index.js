40fefa5dfd62733445760a37675e2bb71911523c
karenhuang925 <92420431+karenhuang925@users.noreply.github.com>
2022-05-07 15:11:25 -0700

    phase12

---
+++ authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+module.exports = router;
